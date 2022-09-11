87d43e9cde8e82f7c6c6f8eb9c3e24548cb6c920
Ilgiz Temi <ilgiztemi@Ilgizs-MacBook-Pro.local>
2022-09-11 16:00:47 -0400

    update server.js

--- server.js
+++ server.js
@@ -56,12 +56,12 @@ app.delete( '/delete/:id', ( req, res ) => {
         }
     } )
 } )
-// if ( process.env.NODE_ENV === 'production' ) {
-//     app.use( express.static( 'client/build' ) )
-//     app.get( "*", ( req, res ) => {
-//         res.sendFile( path.resolve( __dirname, "client", "build", "index.html" ) );
-//     } )
-// }
+if ( process.env.NODE_ENV === 'production' ) {
+    app.use( express.static( 'client/build' ) )
+    app.get( "*", ( req, res ) => {
+        res.sendFile( path.resolve( __dirname, "client", "build", "index.html" ) );
+    } )
+}
 
 app.listen( port, () => {
     console.log( "express is running"+` ${port}` )
